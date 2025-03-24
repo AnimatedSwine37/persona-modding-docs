1fe90690fa35886ae2532a5c97a78196b19d8225
TheBestAstroNOT <139786546+TheBestAstroNOT@users.noreply.github.com>
2025-03-24 17:00:04 +0530

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://thebestastronot.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/persona-modding-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
